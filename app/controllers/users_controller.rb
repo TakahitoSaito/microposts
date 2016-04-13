fa984e4fc6973e7a1921cb6caf3fffbd5d4a773a
saigorian <t.saitoh59@gmail.com>
2016-04-13 13:05:59 +0000

    following-followers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy,
+                                        :following, :followers]
   before_action :set_user, only: [:edit, :update, :show]
   before_action :correct_user, only: [:edit, :update]
 
@@ -35,10 +37,24 @@ class UsersController < ApplicationController
     end
   end
   
+  def followings
+    @title = "Followings"
+    @user  = User.find(params[:id])
+    @users = @user.following_users
+    render 'show_follow'
+  end
+
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.follower_users
+    render 'show_follow'
+  end
+  
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation, :area, :birthday, :birthday_02)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :area,:birthday_02)
   end
   
   def set_user
@@ -49,4 +65,5 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     redirect_to root_path , notice: '不正なアクセスです。' unless @user == current_user 
   end  
+  
 end
